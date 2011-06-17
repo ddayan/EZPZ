6ee10feb6e009b0c965800b6c8cf21d11da01774
Daniel Cater <djcater@gmail.com>
2011-06-17 20:58:52 +0100

    Add buttons to flip between the start screen and the browsing screen.

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const url = require("url");
+const fullscreen = require("fullscreen");
+//const winUtils = require("window-utils");
 
-function navigate() {
-    // invoked when the user hits the go button or hits enter in url box
-    var input = $.trim($("#locationBar").val());
-    // let's try to turn user input into a well formed url using the
-    // 'url' library's guess function.
-    input = url.guess(input);
+$(document).ready(function() {
+    function navigate() {
+        // invoked when the user hits the go button or hits enter in url box
+        var input = $.trim($("#locationBar").val());
+        // let's try to turn user input into a well formed url using the
+        // 'url' library's guess function.
+        input = url.guess(input);
 
-    // now trigger navigation
-    $("#contentFrame").attr("src", input);
-}
+        // now trigger navigation
+        $("#contentFrame").attr("src", input);
+    }
+
+    fullscreen.enable();
+    //winUtils.windowIterator().next().maximize();
+
+    $("#location").submit(function() {
+        navigate();
+        return false;
+    });
+
+    $("#browse").click(function() {
+        $("#startPage").hide();
+        $("#browserHeader").show();
+        $("#browserContentArea").show();
+        $("#browserFooter").show();
+    });
+
+    $("#home").click(function() {
+        $("#startPage").show();
+        $("#browserHeader").hide();
+        $("#browserContentArea").hide();
+        $("#browserFooter").hide();
+    });
+
+});
 
